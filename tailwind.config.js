1a307772fcb5f70a5ae7a7b7ffce686d223b856a
Siss <silvia.krajnakova@gmail.com>
2025-01-21 10:49:03 +0100

    Initial commit with HTML and Tailwind Css done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/input.css',
+    './dist/output.css',
+    './dist/main.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
